02b7ad6a60e87fea162bff55203d7eab6c69910d
Ahmad N. Raja <ar@penneo.com>
2018-03-06 19:51:37 +0100

    Removing the assets

--- blog/site.hs
+++ blog/site.hs
@@ -27,9 +27,9 @@ main = hakyll $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "posts/*/assets/*" $ do
-        route   idRoute
-        compile copyFileCompiler
+    -- match "posts/*/assets/*" $ do
+    --     route   idRoute
+    --     compile copyFileCompiler
 
     match (fromList ["about.md"]) $ do
         route   $ setExtension "html"
